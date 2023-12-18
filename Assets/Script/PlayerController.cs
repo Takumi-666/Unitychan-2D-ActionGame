8446508baa7438ac437716231b572c95d234ede5
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-18 22:41:44 +0900

    プレイヤーの移動

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // 左右の移動
+        float moveInput = 0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveInput = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveInput = 1f;
+        }
+
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // ジャンプ
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 地面との衝突を検知
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // 地面から離れたことを検知
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+
+}
--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -1,18 +1,55 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private bool isJump;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Player movement
+        float x = 0f;
+        if (Input.GetKey(KeyCode.A)) // Left movement with 'A'
+        {
+            x = -1f;
+            Debug.Log("左");
+        }
+        else if (Input.GetKey(KeyCode.D)) // Right movement with 'D'
+        {
+            x = 1f;
+            Debug.Log("→");
+        }
+        rb.velocity = new Vector2(x * 8f, rb.velocity.y);
+
+        // Jump handling
+        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+        {
+            isJump = true;
+            rb.AddForce(new Vector2(0, 7), ForceMode2D.Impulse);
+            Debug.Log("Jumping");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJump = false;
+        }
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+
+        }
     }
 }
