3ea20457568fcf07f456de92a53d2952fe2269ae
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-19 17:55:41 +0900

    コインの取得、スコアの表示機能の追加

---
+++ Assets/Script/CoinCollector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CoinCollector : MonoBehaviour
+{
+    public int scoreValue = 100;
+    public ScoreManager scoreManager; // ScoreManagerへの参照
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            scoreManager.AddScore(scoreValue);
+            gameObject.SetActive(false);
+        }
+    }
+}
+
---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // UI要素を使用するために必要
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score; // スコアを保持する変数
+    public Text scoreText; // スコアを表示するテキスト
+
+    void Start()
+    {
+        score = 0; // スコアの初期化
+        UpdateScore(); // スコア表示の更新
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue; // スコアの追加
+        UpdateScore(); // スコア表示の更新
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score; // スコアのテキストを更新
+    }
+}
