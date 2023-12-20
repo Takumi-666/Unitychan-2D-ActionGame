75e4deeb915ffe93ea613a7294d5e7947e48ac46
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-20 16:45:56 +0900

    BGM,ボタン選択の効果音の追加

---
+++ Assets/Script/BGMManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    private static BGMManager instance = null;
+
+    public static BGMManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            // 既にインスタンスが存在する場合、この新しいインスタンスを破棄
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+
+        // このGameObjectをシーン遷移で破棄されないように設定
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Assets/Script/SelectSound.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SelectSound : MonoBehaviour
+{
+    public AudioClip soundEffect; // 再生するサウンドエフェクト
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // AudioSourceコンポーネントの取得、なければ追加
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    // サウンドを再生し、指定されたシーンに遷移する
+    public void PlayAndLoadScene(string sceneName)
+    {
+        StartCoroutine(PlaySoundAndLoadScene(sceneName));
+    }
+
+    private IEnumerator PlaySoundAndLoadScene(string sceneName)
+    {
+        if (soundEffect != null)
+        {
+            audioSource.PlayOneShot(soundEffect);
+            // サウンドエフェクトの長さだけ待つ
+            yield return new WaitForSeconds(soundEffect.length);
+        }
+        SceneManager.LoadScene(sceneName);
+    }
+}
+
