b4509230e9226d944bdc813910205c747afbae34
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-21 00:57:21 +0900

    Unityちゃんインストール、キャラクターの移動のアニメーション追加

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,69 +1,97 @@
 ﻿using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
-    public GameObject gameClearPanel; // ゲームクリアパネル
-    public GameObject gameOverPanel;  // ゲームオーバーパネル
+    //インスペクターで設定する
+    public float speed; //速度
+    public float gravity; //重力
+    public float jumpSpeed;//ジャンプする速度
+    public float jumpHeight;//高さ制限
+    public float jumpLimitTime;//ジャンプ制限時間
+    public GroundCheck ground; //接地判定
+    public GroundCheck head;//頭ぶつけた判定
 
-    private Rigidbody2D rb;
-    private bool isGrounded;
-
+    //プライベート変数
+    private Animator anim = null;
+    private Rigidbody2D rb = null;
+    private bool isGround = false;
+    private bool isJump = false;
+    private bool isHead = false;
+    private float jumpPos = 0.0f;
+    private float jumpTime = 0.0f;
 
     void Start()
     {
+        //コンポーネントのインスタンスを捕まえる
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        // ゲーム開始時にパネルを非表示にする
-        gameClearPanel.SetActive(false);
-        gameOverPanel.SetActive(false);
     }
 
     void FixedUpdate()
     {
-        // 左右の移動
-        float moveInput = 0f;
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveInput = -1f;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            moveInput = 1f;
-        }
+        //接地判定を得る
+        isGround = ground.IsGround();
+        isHead = head.IsGround();
 
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        //キー入力されたら行動する
+        float horizontalKey = Input.GetAxis("Horizontal");
+        float xSpeed = 0.0f;
+        float ySpeed = -gravity;
 
-        // ジャンプ
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-        }
-    }
+        // スペースキー入力をチェック
+        bool jumpKey = Input.GetKey(KeyCode.Space);
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (isGround)
         {
-            isGrounded = true;
+            if (jumpKey)
+            {
+                ySpeed = jumpSpeed;
+                jumpPos = transform.position.y; //ジャンプした位置を記録する
+                isJump = true;
+                jumpTime = 0.0f;
+            }
+            else
+            {
+                isJump = false;
+            }
         }
-        else if (collision.gameObject.CompareTag("Goal"))
+        else if (isJump)
         {
-            // ゲームクリアパネルを表示
-            gameClearPanel.SetActive(true);
-        }
-        else if (collision.gameObject.CompareTag("Enemy"))
-        {
-            // ゲームオーバーパネルを表示
-            gameOverPanel.SetActive(true);
-        }
-    }
+            // スペースキーを押し続けているか
+            bool pushUpKey = jumpKey;
+            // その他の条件は変更なし
+            bool canHeight = jumpPos + jumpHeight > transform.position.y;
+            bool canTime = jumpLimitTime > jumpTime;
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
+            if (pushUpKey && canHeight && canTime && !isHead)
+            {
+                ySpeed = jumpSpeed;
+                jumpTime += Time.deltaTime;
+            }
+            else
+            {
+                isJump = false;
+                jumpTime = 0.0f;
+            }
         }
+        if (horizontalKey > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            anim.SetBool("Run", true);
+            xSpeed = speed;
+        }
+        else if (horizontalKey < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetBool("Run", true);
+            xSpeed = -speed;
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+            xSpeed = 0.0f;
+        }
+        anim.SetBool("Jamp", isJump); //New
+        anim.SetBool("Ground", isGround); //New
+        rb.velocity = new Vector2(xSpeed, ySpeed);
     }
 }
