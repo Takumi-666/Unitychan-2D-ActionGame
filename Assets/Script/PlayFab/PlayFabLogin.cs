197b3175b16fdcf043d681e40744e0c72b134bc2
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-21 04:28:48 +0900

    playfabのランキング情報の取得、表示

---
+++ Assets/Script/PlayFab/PlayFabLogin.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayFabLogin : MonoBehaviour
+{
+    void Start()
+    {
+        PlayFabAuthService.Instance.Authenticate(Authtypes.Silent);
+    }
+
+    private void OnEnable()
+    {
+        PlayFabAuthService.OnLoginSuccess += PlayFabAuthService_OnLoginSuccess;
+        PlayFabAuthService.OnPlayFabError += PlayFabAuthService_OnPlayFabError;
+    }
+
+    private void OnDisable()
+    {
+        PlayFabAuthService.OnLoginSuccess -= PlayFabAuthService_OnLoginSuccess;
+        PlayFabAuthService.OnPlayFabError -= PlayFabAuthService_OnPlayFabError;
+    }
+
+    private void PlayFabAuthService_OnLoginSuccess(LoginResult success)
+    {
+        Debug.Log("ログイン成功");
+    }
+
+    private void PlayFabAuthService_OnPlayFabError(PlayFabError error)
+    {
+        Debug.Log("ログイン失敗: " + error.GenerateErrorReport());
+    }
+}
---
+++ Assets/Script/PlayFab/PlayFabRankingPlayFabRanking.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+using System.Text;
+
+public class PlayFabRanking : MonoBehaviour
+{
+    public Text Nameinput; // Nameinput用のText UI要素
+    public Text Scoreinput; // Scoreinput用のText UI要素
+
+    void Start()
+    {
+        GetLeaderboard();
+    }
+
+    public void GetLeaderboard()
+    {
+        PlayFabClientAPI.GetLeaderboard(new GetLeaderboardRequest
+        {
+            StatisticName = "test",
+            StartPosition = 0, // ランキングの開始位置
+            MaxResultsCount = 10 // 取得する最大結果数
+        }, result =>
+        {
+            StringBuilder names = new StringBuilder();
+            StringBuilder scores = new StringBuilder();
+
+            foreach (var item in result.Leaderboard)
+            {
+                string displayName = item.DisplayName ?? "NoName";
+                names.AppendLine($"{item.Position + 1}位: {displayName}");
+                scores.AppendLine($"スコア {item.StatValue}");
+            }
+
+            // Text UI要素に値を設定
+            Nameinput.text = names.ToString();
+            Scoreinput.text = scores.ToString();
+        }, error =>
+        {
+            Debug.Log("ランキングデータ取得失敗: " + error.GenerateErrorReport());
+        });
+    }
+}
