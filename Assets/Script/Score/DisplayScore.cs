b93d428976ff0dd2fdcc56e97e3180b102c4d261
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-22 21:13:33 +0900

    スコアシーンでのスコアの表示

---
+++ Assets/Script/Score/DisplayScore.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayScore : MonoBehaviour
+{
+    public Text scoreText;
+
+    void Start()
+    {
+        // 保存されたスコアを表示
+        scoreText.text = "Score: " + ScoreManager.score;
+    }
+}
