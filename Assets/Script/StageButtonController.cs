ebba81424e87ebea255ad08f8a19f6d884000535
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-22 19:36:55 +0900

    ステージ選択シーン UI（仮）

---
+++ Assets/Script/StageButtonController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class StageButtonController : MonoBehaviour
+{
+    public GameObject square;
+    public GameObject startButton;
+    public GameObject[] texts;
+    public AudioSource audioSource; // AudioSourceの参照
+
+    public void ShowContent(int buttonIndex)
+    {
+        // サウンド再生
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+
+        square.SetActive(true);
+        startButton.SetActive(true);
+
+        foreach (var text in texts)
+        {
+            text.SetActive(false);
+        }
+
+        if (buttonIndex >= 0 && buttonIndex < texts.Length)
+        {
+            texts[buttonIndex].SetActive(true);
+        }
+    }
+}
