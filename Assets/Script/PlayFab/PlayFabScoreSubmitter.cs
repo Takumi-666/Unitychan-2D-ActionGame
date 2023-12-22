46aa55488680e59a06c18b5af2679d08f8a565d9
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-23 02:19:13 +0900

    スコア登録の処理

---
+++ Assets/Script/PlayFab/PlayFabScoreSubmitter.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+using System.Collections.Generic;
+
+public class PlayFabSubmitter : MonoBehaviour
+{
+    public InputField usernameInput; // ユーザーネーム入力用のInputField
+
+    public void SubmitScore()
+    {
+        string username = usernameInput.text;
+        if (username.Length < 2 || username.Length > 8)
+        {
+            Debug.Log("ユーザーネームは2文字以上8文字以下でお願いします");
+            return;
+        }
+
+        UpdatePlayerStatistics();
+        UpdateDisplayName(username);
+    }
+
+
+    private void UpdatePlayerStatistics()
+    {
+        var request = new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate>
+        {
+            new StatisticUpdate { StatisticName = "test", Value = ScoreManager.score }
+        }
+        };
+        PlayFabClientAPI.UpdatePlayerStatistics(request, OnStatisticsUpdate, OnPlayFabError);
+    }
+
+
+    private void UpdateDisplayName(string displayName)
+    {
+        var request = new UpdateUserTitleDisplayNameRequest { DisplayName = displayName };
+        PlayFabClientAPI.UpdateUserTitleDisplayName(request, OnDisplayNameUpdate, OnPlayFabError);
+    }
+
+    private void OnStatisticsUpdate(UpdatePlayerStatisticsResult result)
+    {
+        Debug.Log("スコア更新成功");
+    }
+
+    private void OnDisplayNameUpdate(UpdateUserTitleDisplayNameResult result)
+    {
+        Debug.Log("表示名更新成功");
+    }
+
+    private void OnPlayFabError(PlayFabError error)
+    {
+        Debug.LogError("PlayFabエラー: " + error.GenerateErrorReport());
+    }
+}
