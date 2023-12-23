e179c07db23cecb5b5748edc0a656a182a99bc97
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-23 17:35:10 +0900

    スコアシーン、Reスタートボタンの微調整

---
+++ Assets/Script/PanelController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+public class PanelController : MonoBehaviour
+{
+    public GameObject panel;
+    public AudioClip sound; // 再生するサウンドエフェクト
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.clip = sound; // クリップを設定
+    }
+
+    // ボタンクリック時に呼び出されるメソッド
+    public void TogglePanel()
+    {
+        bool isPanelActive = panel.activeSelf;
+        panel.SetActive(!isPanelActive); // パネルの表示状態を切り替え
+        audioSource.Play(); // 効果音を再生
+    }
+}
