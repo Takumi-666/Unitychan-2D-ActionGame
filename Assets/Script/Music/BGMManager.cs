fcc143403169e59614874fc107a0abf2591be8f2
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-23 16:36:46 +0900

    全シーンのBGMの処理　官僚

--- Assets/Script/Music/BGMManager.cs
+++ Assets/Script/Music/BGMManager.cs
@@ -1,26 +1,56 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BGMManager : MonoBehaviour
 {
     private static BGMManager instance = null;
-
-    public static BGMManager Instance
-    {
-        get { return instance; }
-    }
+    public AudioSource audioSource;
+    public AudioClip[] bgmClips; // シーンごとのBGMクリップ
 
     void Awake()
     {
         if (instance != null && instance != this)
         {
-            // 既にインスタンスが存在する場合、この新しいインスタンスを破棄
             Destroy(this.gameObject);
             return;
         }
 
         instance = this;
-
-        // このGameObjectをシーン遷移で破棄されないように設定
         DontDestroyOnLoad(this.gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        int sceneNumber = GetSceneNumber(scene.name);
+        if (audioSource.clip != bgmClips[sceneNumber])
+        {
+            audioSource.clip = bgmClips[sceneNumber];
+            audioSource.Play();
+        }
+    }
+
+int GetSceneNumber(string sceneName)
+{
+    switch (sceneName)
+    {
+        case "TitleScene":
+        case "InstructionsScene":
+        case "StageSelectScene":
+            return 0; // ➀のBGM
+        case "1StageScene":
+            return 1; // ➁のBGM
+        case "ScoreScene":
+        case "RankingScene":
+            return 2; // ③のBGM
+        default:
+            return -1; // 該当しない場合
+    }
+}
+
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
